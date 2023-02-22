ce564578e20fe1f06f50bb73e07533b85c1ad802
esJohan <imjohanjjcc@gmail.com>
2023-02-21 23:33:30 -0600

    v1

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const imgDice = document.querySelector('.dice');
+const scorePlayer1 = document.querySelector('#name--1');
+const scorePlayer0 = document.querySelector('#name--0');
+const current1 = document.querySelector('#current--1');
+const current0 = document.querySelector('#current--0');
+const ttlHSre1 = document.querySelector('#score--1');
+const ttlHSre0 = document.querySelector('#score--0');
+let currentPlayer = 0;
+let currentScore = 0;
+
+//Function to Roll Dice//
+const FunforDice = function () {
+  let rdnNumber = Math.floor(Math.random() * 6) + 1;
+  currentScore += rdnNumber;
+  imgDice.setAttribute('src', `dice-${rdnNumber}.png`);
+  if (currentPlayer == 0) current0.textContent = currentScore.toString();
+  if (currentPlayer == 1) current1.textContent = currentScore.toString();
+  if (rdnNumber == 1) PlayerSwticher();
+};
+/////////////////////////////////////////////////////////FUNCTION TO HOLD////////////////////////////////////////////////////////
+
+function toHold() {
+  if (currentPlayer == 0) {
+    let toAdd = parseInt(current0.textContent);
+    let hs0 = parseInt(ttlHSre0.textContent);
+    hs0 += toAdd;
+    alert(hs0);
+    ttlHSre0.textContent = hs0;
+    PlayerSwticher();
+  } else if (currentPlayer == 1) {
+    let toAdd1 = parseInt(current1.textContent);
+    let hs1 = parseInt(ttlHSre1.textContent);
+    hs1 += toAdd1;
+    alert(hs1);
+    ttlHSre1.textContent = hs1;
+    PlayerSwticher();
+  }
+  toAdd = 0;
+  hs0 = 0;
+}
+//TODO:Player Switcher//
+function PlayerSwticher() {
+  alert('Is the next player turn');
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  currentScore = 0;
+}
+
+btnRollDice.addEventListener('click', function () {
+  FunforDice();
+});
+
+btnHold.addEventListener('click', function () {
+  toHold();
+});
