8d47230461a8ed7c805392bf63ade733bb2b5511
esJohan <imjohanjjcc@gmail.com>
2023-02-23 00:38:32 -0600

    fullButCGB

--- script.js
+++ script.js
@@ -11,50 +11,59 @@ const lblPlayer1 = document.querySelector('.player--1');
 const lblPlayer0 = document.querySelector('.player--0');
 let currentPlayer = 0;
 let currentScore = 0;
+let gamePlaying = true;
 
 //Function to Roll Dice//
 const FunforDice = function () {
-  let rdnNumber = Math.floor(Math.random() * 6) + 1;
-  currentScore += rdnNumber;
-  imgDice.setAttribute('src', `dice-${rdnNumber}.png`);
-  if (currentPlayer == 0) current0.textContent = currentScore.toString();
-  if (currentPlayer == 1) current1.textContent = currentScore.toString();
-  if (rdnNumber == 1) PlayerSwticher();
+  if (gamePlaying) {
+    let rdnNumber = Math.floor(Math.random() * 6) + 1;
+    currentScore += rdnNumber;
+    imgDice.setAttribute('src', `dice-${rdnNumber}.png`);
+    if (currentPlayer == 0) current0.textContent = currentScore.toString();
+    if (currentPlayer == 1) current1.textContent = currentScore.toString();
+    if (rdnNumber == 1) PlayerSwticher();
+  }
 };
 /////////////////////////////////////////////////////////FUNCTION TO HOLD////////////////////////////////////////////////////////
 
 function toHold() {
-  if (currentPlayer == 0) {
-    let toAdd = parseInt(current0.textContent);
-    let hs0 = parseInt(ttlHSre0.textContent);
-    hs0 += toAdd;
-    alert(hs0);
-    ttlHSre0.textContent = hs0;
-    if (parseInt(ttlHSre0.textContent) >= 20) {
-      lblPlayer0.classList.add('player--winner');
-      lblPlayer0.classList.remove('player--active');
-    } else {
-      PlayerSwticher();
+  if (gamePlaying) {
+    if (currentPlayer == 0) {
+      let toAdd = parseInt(current0.textContent);
+      let hs0 = parseInt(ttlHSre0.textContent);
+      hs0 += toAdd;
+      alert(hs0);
+      ttlHSre0.textContent = hs0;
+      if (parseInt(ttlHSre0.textContent) >= 20) {
+        lblPlayer0.classList.add('player--winner');
+        lblPlayer0.classList.remove('player--active');
+        imgDice.classList.add('hidden');
+        gamePlaying = false;
+      } else {
+        PlayerSwticher();
+      }
+      toAdd = 0;
+      hs0 = 0;
+    } else if (currentPlayer == 1) {
+      let toAdd1 = parseInt(current1.textContent);
+      let hs1 = parseInt(ttlHSre1.textContent);
+      hs1 += toAdd1;
+      alert(hs1);
+      ttlHSre1.textContent = hs1;
+      if (parseInt(ttlHSre1.textContent) >= 20) {
+        lblPlayer1.classList.add('player--winner');
+        lblPlayer1.classList.remove('player--active');
+        imgDice.classList.add('hidden');
+        gamePlaying = false;
+      } else {
+        PlayerSwticher();
+      }
+      toAdd1 = 0;
+      hs1 = 0;
     }
-    toAdd = 0;
-    hs0 = 0;
-  } else if (currentPlayer == 1) {
-    let toAdd1 = parseInt(current1.textContent);
-    let hs1 = parseInt(ttlHSre1.textContent);
-    hs1 += toAdd1;
-    alert(hs1);
-    ttlHSre1.textContent = hs1;
-    if (parseInt(ttlHSre1.textContent) >= 20) {
-      lblPlayer1.classList.add('player--winner');
-      lblPlayer1.classList.remove('player--active');
-    } else {
-      PlayerSwticher();
-    }
-    toAdd1 = 0;
-    hs1 = 0;
   }
 }
-//TODO:Player Switcher//
+//:Player Switcher//
 function PlayerSwticher() {
   alert('Is the next player turn');
   document.getElementById(`current--${currentPlayer}`).textContent = 0;
@@ -65,6 +74,24 @@ function PlayerSwticher() {
   lblPlayer0.classList.toggle('player--active');
 }
 
+//TODO://///RESET GAME //////////////////////
+const res = function reset() {
+  currentPlayer = 0;
+  currentScore = 0;
+  gamePlaying = true;
+
+  current0.textContent = '0';
+  current1.textContent = '0';
+  ttlHSre0.textContent = '0';
+  ttlHSre1.textContent = '0';
+
+  imgDice.classList.remove('hidden');
+  lblPlayer0.classList.remove('player--winner');
+  lblPlayer0.classList.add('player--active');
+  lblPlayer1.classList.remove('player--winner');
+  lblPlayer1.classList.remove('player--active');
+};
+
 btnRollDice.addEventListener('click', function () {
   FunforDice();
 });
@@ -72,3 +99,4 @@ btnRollDice.addEventListener('click', function () {
 btnHold.addEventListener('click', function () {
   toHold();
 });
+btnNewGame.addEventListener('click', res);
